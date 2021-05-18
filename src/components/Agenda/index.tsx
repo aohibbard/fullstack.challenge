a03b858743b92cd9a534c6fadaebfcbd342c3e3b
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2021-05-18 22:25:01 +0100

    upddates agenda index

--- src/components/Agenda/index.tsx
+++ src/components/Agenda/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext, useMemo } from 'react'
+import React, { ReactElement, useContext, useMemo, useState, useEffect } from 'react'
 import { DateTime } from 'luxon'
 
 import greeting from 'lib/greeting'
@@ -29,17 +29,51 @@ const compareByDateTime = (a: AgendaItem, b: AgendaItem) =>
 const Agenda = (): ReactElement => {
   const account = useContext(AccountContext)
 
+  //add departments and calendars to array with account, create filters based onthose.
   const events: AgendaItem[] = useMemo(
     () =>
       account.calendars
-        .flatMap((calendar) =>
+        .map(calendar =>
           calendar.events.map((event) => ({ calendar, event })),
-        )
+        ).flat()
         .sort(compareByDateTime),
     [account],
   )
 
+  //not optimal solution to update time
+  useEffect(() => {
+    const timer = setTimeout(() => 60000)
+  }, [])
+
   const title = useMemo(() => greeting(DateTime.local().hour), [])
+
+  //updating and selecting calendars
+  const [visibleCalendars, selectCalendars] = useState([]))
+  function filterCalendars(e:any){
+    if (visibleCalendars.length === account.calendars.length){
+      selectCalendars(visibleCalendars.filter(cal => cal === e.target.id))
+    } else if (visibleCalendars.includes(e.target.id)){
+      selectCalendars(visibleCalendars.filter(cal => cal !== e.target.id))
+    } else {
+      selectCalendars([...visibleCalendars, e.target.id])
+    } 
+
+    if (visibleCalendars.length === 0){
+      selectCalendars(account.calendars.map(cal => cal.id))
+    }
+  }
+
+  const [departments, selectDepartments] = useState([])
+  const departmentList = Array.from(new Set(events.map(ev => {
+    if(ev.event.department){
+      ev.event.department
+    }
+  })))
+  // function filterDepartments(e:any){
+  //     reacte logic similar to above to update the array of selected departments
+  //     filter the events useMemo function with this information
+  // }
+
 
   return (
     <div className={style.outer}>
@@ -47,6 +81,25 @@ const Agenda = (): ReactElement => {
         <div className={style.header}>
           <span className={style.title}>{title}</span>
         </div>
+
+        <form>
+          <p>Filter Calendars</p>
+          { account.calendars.map( cal => (
+            <label>
+              <input type="checkbox" key={cal.id} id={cal.color} name={cal.color} onChange={(e) => filterCalendars(e)} />
+              {cal.color}
+            </label>
+          ) }
+        </form>
+          // <p>Filter Departments</p>
+          // { //departmentList.map(deptartmentName => {
+          //     <label>
+          //     <input type="checkbox" onChange={() => //filterDepartments()} />
+          //     {deptartmentName}
+          //   </label>
+          //   })
+          //}
+
 
         <List>
           {events.map(({ calendar, event }) => (
